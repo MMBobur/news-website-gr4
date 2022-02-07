1429104eb853938acd3a6b0c1251c4901f4f14d9
MuzaffarJuraev <juraev_m2000@mail.ru>
2022-02-07 19:55:53 +0500

    end of category

--- src/App.js
+++ src/App.js
@@ -5,12 +5,10 @@ import User from "./pages/User";
 import Category from "./pages/Category";
 import News from "./pages/News";
 import NoMatch from "./pages/404";
-import TemporaryDrawer from "./components/Drawer";
 
 function App() {
   return (
     <>
-      <TemporaryDrawer />
       <Routes>
         <Route index element={<Home />} />
         <Route path="user" element={<User />} />
